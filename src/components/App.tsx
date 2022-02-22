1059d24e1b7ed5f6dbaa5af42dec304f0d7e7808
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2022-02-22 01:24:54 +0100

    initialize

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="md:container mx-auto flex items-center justify-center h-screen bg-gray-200">
+      <p className=" text-indigo-400 text-2xl">React + Tailwind + Typescript + Electron = ❤</p>
+    </div>
+  );
+};
+
+export default App;
